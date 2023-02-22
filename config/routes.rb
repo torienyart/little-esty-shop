1e67ccfd33e9806bc55448acecdf237f5dc63f4c
Brad Dunlap <braddunlapdpt@gmail.com>
2023-02-22 18:45:10 -0500

    refactor user story 1 and 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get "/admin", to: 'admin#index'
-
-
-	resources :merchants do
-		member do 
-			get 'dashboard'
-		end
-	end
+	 
+	get "/merchants/:id/dashboard", to: 'merchants#show'
 
   namespace :admin do
     resources :merchants, only: [:index]
--- spec/features/merchant/dashboard_spec.rb
+++ spec/features/merchant/dashboard_spec.rb
@@ -1,19 +1,16 @@
 RSpec.describe 'Merchant Dashboard' do
 	before(:each) do
 		@merchant = Merchant.create!(name: "Carlos Jenkins") 
+		visit "/merchants/#{@merchant.id}/dashboard"
 	end
 
   describe 'As a user when I visit my merchant dashboard' do
     it 'I see the name of my merchant' do 
-      visit "/merchants/#{@merchant.id}/dashboard"
 
       expect(page).to have_content("Welcome to your dashboard, Carlos Jenkins")
     end
 
     it 'see link to my merchant items index and merchant invoices index' do 
-      visit "/merchants/#{@merchant.id}/dashboard"
-			save_and_open_page
-			
 
       expect(page).to have_link("My Merchant Items Index", href: "/merchants/#{@merchant.id}")
       expect(page).to have_link("My Merchant Invoices Index", href: "/merchants/#{@merchant.id}/invoices")
