e41ec13edf0bdf59720f5339df7db5ef58508a99
Conner Van Loan <cdvanloan@gmail.com>
2023-03-03 13:36:50 -0700

    adds tests and passing features for us4 bd show page

---
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Bulk Discount Show' do
+	before(:each) do
+		Merchant.destroy_all
+		Customer.destroy_all
+		Invoice.destroy_all
+		Item.destroy_all
+		Transaction.destroy_all
+		InvoiceItem.destroy_all
+		@merchant = Merchant.create!(name: "Carlos Jenkins") 
+		
+    
+    @bogo = @merchant.bulk_discounts.create!(percentage_discount: 50, quantity_threshold: 100, name: "BOGO")
+    @holiday = @merchant.bulk_discounts.create!(percentage_discount: 20, quantity_threshold: 10, name: "Holiday Sale")
+
+    visit "/merchants/#{@merchant.id}/bulk_discounts/#{@bogo.id}"
+	end
+
+  describe 'As a merchant when I visit a bulk discount show page' do
+    it 'displays the bulk discount and its details' do
+      expect(page).to have_content("Bulk Discount: #{@bogo.name}")
+      expect(page).to have_content("Percent Discount: 50%")
+      expect(page).to have_content("Quantity Threshold: 100")
+      expect(page).to_not have_content @holiday.name
+    end
+  end
+
+
+end
