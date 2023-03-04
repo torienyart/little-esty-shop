3408c04aec1e3c253cb506f2291de70efe29c62f
Tori Enyart <torienyart@gmail.com>
2023-03-04 12:26:35 -0700

    Add relationships tests for new bulk_discount relationships

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Invoice, type: :model do
   describe "relationships" do
     it { should belong_to :customer }
     it { should have_many :transactions }
+    it { should have_many(:items).through(:invoice_items) }
     it { should have_many :invoice_items }
     it { should have_many(:merchants).through(:items) }
 
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe Merchant, type: :model do
     it { should have_many :items }
     it { should have_many(:invoice_items).through(:items) }
     it { should have_many(:invoices).through(:invoice_items) }
-    it { should validate_presence_of :name }
+    it { should have_many(:customers).through(:invoices) }
+		it { should have_many(:transactions).through(:invoices) }
 		it { should have_many(:bulk_discounts) }
+    it { should validate_presence_of :name }
 	
   end
 
